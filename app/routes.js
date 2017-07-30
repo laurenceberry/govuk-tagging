97358b1257cc3bd5e3c8ac667ccf478f7a5d24e2
laurenceberry <berry.laur@gmail.com>
2017-07-30 20:04:11 +0100

    Build basic tagging

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var router = express.Router()
+var fs = require('fs');
+var yaml = require('js-yaml');
+var express = require('express');
+var router = express.Router();
 
 // Route index page
 router.get('/', function (req, res) {
@@ -9,3 +11,21 @@ router.get('/', function (req, res) {
 // add your routes here
 
 module.exports = router
+
+router.get('/tagger/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  res.render(
+    "tagger/" + req.params.page,
+    templateData
+  );
+
+});
